1d108ffc8a3c00c2073ca4aaeb26dddd742b8b49
TylorDev <jhontmg26@gmail.com>
2024-03-27 20:05:20 -0300

    navbar terminado

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import navbar from "./../Styles/Components/Navbar.module.scss";
-
+import { TbLockSquareRounded } from "react-icons/tb";
+import { FaRegUser } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 /*
 Para usar  css modules usa esta expesiones regulares al terminar de trabajar aqui.
 
@@ -11,28 +13,31 @@ Remplazar = className={navbar.$1}
 export default function Navbar() {
   return (
     <nav className={navbar.navbar}>
-      <div className={navbar.navbarLogo}>
-        {/* Aquí puedes colocar tu logo */}
+      <div className={navbar.Logo}>
         <img src="./logo.png" alt="Logo" />
       </div>
-      <div className={navbar.navbarSearch}>
-        {/* Aquí puedes colocar tu input searchbar */}
-        <input type="text" placeholder="Search..." />
+      <div className={navbar.Search}>
+        <input type="text" placeholder="Search" />
+        <FiSearch className={navbar.lupa} />
       </div>
-      <div className={navbar.navbarLinks}>
-        {/* Aquí puedes colocar los enlaces de tu navbar */}
+      <div className={navbar.Links}>
         <a href="#">Explore</a>
         <a href="#">Stats</a>
         <a href="#">Resources</a>
         <a href="#">Create</a>
       </div>
-      <div className={navbar.navbarProfile}>
-        {/* Aquí puedes colocar el botón de perfil */}
-        <button>Profile</button>
-      </div>
-      <div className={navbar.navbarWallet}>
-        {/* Aquí puedes colocar el botón de wallet */}
-        <button>Wallet</button>
+
+      <div className={navbar.buttons}>
+        <div className={navbar.Profile}>
+          <button>
+            <FaRegUser />
+          </button>
+        </div>
+        <div className={navbar.Wallet}>
+          <button>
+            <TbLockSquareRounded />
+          </button>
+        </div>
       </div>
     </nav>
   );
