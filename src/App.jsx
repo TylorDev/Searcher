d22d920e18d68f6bbaaf8e6416027c950cd77883
TylorDev <jhontmg26@gmail.com>
2024-03-30 23:08:53 -0300

    slidder comienzo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Homepage from "./Pages/Homepage";
+import Explore from "./Pages/Explore";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage></Homepage>}></Route>
           <Route path="*" element={<div> ERROR: 404 </div>}></Route>
+          <Route path="/Explore" element={<Explore></Explore>}></Route>
         </Routes>
         <Footer></Footer>
       </BrowserRouter>
